58f98f04314bd76c8b6851cc9b3c8f13a1111a5d
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-03-25 02:14:20 +0100

    fix(all): bug fixes

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,9 @@ export function configure(frameworkConfig: FrameworkConfiguration) {
   const bindingBehaviorInstance = frameworkConfig.container.get(ObservableSignalBindingBehavior)
   viewResources.registerBindingBehavior('observableSignal', bindingBehaviorInstance)
   
-  const bindingFunctionInstance = frameworkConfig.container.get(RxBindingFunction)
+  const rxBindingFunctionInstance = frameworkConfig.container.get(RxBindingFunction)
   if (typeof viewResources.registerBindingFunction === 'function') {
-    viewResources.registerBindingFunction('@rx', bindingFunctionInstance)
+    viewResources.registerBindingFunction('@rx', rxBindingFunctionInstance)
   } else {
     throw new Error('You need to load the aurelia-binding-functions plugin before aurelia-observable-binding-function.')
   }
@@ -55,7 +55,7 @@ export class RxBindingFunction implements BindingFunction {
     if (observable) {
       if (!observable._aureliaIsListener) { // it's a value
         if (typeof observable.subscribe === 'function') {
-          logger.debug('[evaluate] retrieve last value of:', callScope.args[0].name)
+          logger.debug('[evaluate] retrieve last value of:', callScope.args[0].name, observable.value, observable)
           return observable.value
         }
         logger.error('[evaluate] trying to get a value but no underlying observable exists for:', callScope.args[0].name)
@@ -63,10 +63,9 @@ export class RxBindingFunction implements BindingFunction {
         if (typeof observable.next === 'function') {
           const event = scope.overrideContext.$event
           if (!event) {
-            // callScope simply means it was not an action that was triggered, 
+            // this simply means it was not an action that was triggered, 
             // but that we are binding TO the action, and the system wants its value
-            // it would be safer to do callScope explicitly, i.e.
-            // rxActionShare(action, 'param')
+            // it is safer to bind to the observable directly
             // instead of assuming the person is binding and not evaluating
             return observable
           }
@@ -74,8 +73,8 @@ export class RxBindingFunction implements BindingFunction {
           argsToEval.shift()
           const args = argsToEval.map(arg => arg.evaluate(scope, lookupFunctions))
           logger.debug('[evaluate] trigger an action of:', callScope.args[0].name, {event, args})        
-          observable.next({ event, args, scope })
-          return observable.onTriggerReturn
+          observable.next({ event, arguments: args, scope })
+          return //observable.onTriggerReturn
         } else {
           logger.error('[evaluate] trying to trigger an action but no underlying subject exists:', callScope.args[0].name)
         }
@@ -87,78 +86,90 @@ export class RxBindingFunction implements BindingFunction {
   
   bind(callScope: CallScope, binding: Binding, scope: Scope, lookupFunctions) {
     const observable = callScope.args[0].evaluate(scope, lookupFunctions, true) //as Subject<any>
-    observable._aureliaBindCount = (observable._aureliaBindCount || 0) + 1
+    logger.debug(`[bind] [${callScope.name}]`, callScope.args[0].name, callScope, binding, scope, 'value of target', observable)
     
-    if (binding instanceof Listener) {
-      observable._aureliaIsListener = true
-    } else {
-      if (!observable._aureliaSubscription) {
-        logger.debug('[connect] will subscribe to', callScope.args[0].name)
-        // observable can either emit a ChangeWithMetadata or a simple value
+    if (observable && typeof observable.subscribe === 'function') {
+      binding._observableReference = observable
+      observable._aureliaBindCount = (observable._aureliaBindCount || 0) + 1
+      
+      if (binding instanceof Listener) {
+        observable._aureliaIsListener = true
+      }
+      else if (!observable._aureliaSubscription) {
+        logger.debug('[bind] will subscribe to', callScope.args[0].name)
         
         const subscription = observable
           .subscribe((value) => observable.value = value)
 
         observable._aureliaSubscription = subscription
       }
-    }
-    
-    const getArgs = () => {
-      if (!scope.bindingContext) {
-        logger.error(`[bind-getargs] sorry, can't get args when no bindingContext exists`)
-        return []
+      /*
+      const getArgs = () => {
+        if (!scope.bindingContext) {
+          logger.error(`[bind-getargs] sorry, can't get args when no bindingContext exists`)
+          return []
+        }
+        const argsToEval = Array.from(callScope.args)
+        argsToEval.shift()
+        return evalList(scope, argsToEval, lookupFunctions)
       }
-      const argsToEval = Array.from(callScope.args)
-      argsToEval.shift()
-      return evalList(scope, argsToEval, lookupFunctions)
-    }
-    /* create if non-existent
-    if (observable === undefined) {
-      let newChangable: Subject<any>
-      if (binding instanceof Listener) {
-        logger.debug('[bind] will create a changable EVENT observable for', callScope.args[0].name, 'with args method like', () => evalList(scope, callScope.args, lookupFunctions))
-        newChangable = new Subject()
-        //changableAction(getArgs, aureliaScopeIterator(scope), callScope.args[0].name) // create changableAction
-      } else {
-        logger.debug('[bind] will create a changable VALUE observable for', callScope.args[0].name)
-        newChangable = new ReplaySubject(1) // create empty changable
-        // TODO: perhaps setting initial value via argument??
+      */
+      /* create if non-existent
+      if (observable === undefined) {
+        let newChangable: Subject<any>
+        if (binding instanceof Listener) {
+          logger.debug('[bind] will create a changable EVENT observable for', callScope.args[0].name, 'with args method like', () => evalList(scope, callScope.args, lookupFunctions))
+          newChangable = new Subject()
+          //changableAction(getArgs, aureliaScopeIterator(scope), callScope.args[0].name) // create changableAction
+        } else {
+          logger.debug('[bind] will create a changable VALUE observable for', callScope.args[0].name)
+          newChangable = new ReplaySubject(1) // create empty changable
+          // TODO: perhaps setting initial value via argument??
+        }
+        
+        callScope.args[0].assign(scope, newChangable)
+        logger.debug('[bind] created an observable for', callScope.args[0].name, 'at scope', scope)
+        observable = newChangable
+        // we assert that observable is callScope.args[0].evaluate(scope, lookupFunctions, true)
+      }
+      */
+      /*
+      if (!observable.getInitialArguments) {
+        // in case we pre-created the observable
+        // callScope happens when binding to a value in a different context
+        // since bind() is ready, but no Observable is in context
+        observable.getInitialArguments = getArgs
       }
       
-      callScope.args[0].assign(scope, newChangable)
-      logger.debug('[bind] created an observable for', callScope.args[0].name, 'at scope', scope)
-      observable = newChangable
-      // we assert that observable is callScope.args[0].evaluate(scope, lookupFunctions, true)
+      if (!observable.initialScope)
+        observable.initialScope = scope //aureliaScopeIterator(scope)
+      
+      if (!observable.initialName)
+        observable.initialName = callScope.args[0].name
+      */
     }
-    */
-    if (!observable.getInitialArguments) {
-      // in case we pre-created the observable
-      // callScope happens when binding to a value in a different context
-      // since bind() is ready, but no Observable is in context
-      observable.getInitialArguments = getArgs
-    }
-    
-    if (!observable.initialScope)
-      observable.initialScope = scope //aureliaScopeIterator(scope)
-    
-    if (!observable.initialName)
-      observable.initialName = callScope.args[0].name
-
-    logger.debug(`[bind] [${callScope.name}]`, callScope.args[0].name, callScope, binding, scope, 'value of target', observable)
   }
   
   unbind(callScope: CallScope, binding: Binding, scope: Scope) {
-    let observable = callScope.args[0].evaluate(scope, binding.lookupFunctions, true) // as Subject<any>
+    let observable = binding._observableReference
     
-    console.assert(!observable._aureliaBindCount || observable._aureliaBindCount <= 0, '_aureliaBindCount is wrong')
-    
-    observable._aureliaBindCount = (observable._aureliaBindCount || 0) - 1
-    
-    if (observable._aureliaBindCount === 0 && observable._aureliaSubscription) {
-      logger.debug(`[unbind] [${callScope.name}] unsubscribing`, callScope.args[0].name, callScope, binding, scope, new Date())
-      observable._aureliaSubscription.unsubscribe()
-      observable._aureliaSubscription = undefined
-      observable._aureliaBindCount = undefined
+    if (observable && typeof observable.subscribe === 'function') {
+      console.assert(observable._aureliaBindCount !== undefined && observable._aureliaBindCount > 0, '_aureliaBindCount is wrong')
+      
+      observable._aureliaBindCount = (observable._aureliaBindCount || 0) - 1
+      
+      if (observable._aureliaBindCount === 0 && observable._aureliaSubscription) {
+        logger.debug(`[unbind] [${callScope.name}] unsubscribing`, callScope.args[0].name, callScope, binding, scope, new Date())
+        observable._aureliaSubscription.unsubscribe()
+        observable._aureliaSubscription = undefined
+        observable._aureliaBindCount = undefined
+        observable.value = undefined
+        // observable.initialName = undefined
+        // observable.initialScope = undefined
+        // observable.getInitialArguments = undefined
+      }
+    } else {
+      logger.debug(`[unbind] [${callScope.name}] NOT unsubscribing`, callScope.args[0].name, callScope, binding, scope, new Date())      
     }
   }
 }
@@ -212,7 +223,7 @@ export type AnyEvent = Event | FocusEvent | GamepadEvent | HashChangeEvent | Key
 
 var evalListCache = [[],[0],[0,0],[0,0,0],[0,0,0,0],[0,0,0,0,0]];
 /// Evaluate the [list] in context of the [scope].
-function evalList(scope, list, lookupFunctions) {
+export function evalList(scope, list, lookupFunctions) {
   var length = list.length,
       cacheLength, i;
 
